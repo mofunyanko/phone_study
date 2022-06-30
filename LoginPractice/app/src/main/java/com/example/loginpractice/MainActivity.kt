22142a107f85b6d9fb045a70bff82a6aa141f480
mofunyanko <emiho.iwasaki@gmail.com>
2022-07-01 01:34:52 +0900

    LoginPractice

---
+++ LoginPractice/app/src/main/java/com/example/loginpractice/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.loginpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // FirebaseAuthオブジェクトの共有インスタンスを取得
+        auth = FirebaseAuth.getInstance()
+
+        val buttonSignUp = findViewById<Button>(R.id.SignUpButton)
+        val buttonLogin = findViewById<Button>(R.id.LoginButton)
+
+        buttonSignUp.setOnClickListener {
+            val emailEditText = findViewById<EditText>(R.id.emailEditText)
+            val emailText = emailEditText.text.toString()
+            val passEditText = findViewById<EditText>(R.id.passEditText)
+            val passText = passEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(emailText, passText).addOnCompleteListener(this) {
+                task -> if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "SignUp 成功", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(baseContext, "SignUp 失敗", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        buttonLogin.setOnClickListener {
+            val emailEditText = findViewById<EditText>(R.id.emailEditText)
+            val emailText = emailEditText.text.toString()
+            val passEditText = findViewById<EditText>(R.id.passEditText)
+            val passText = passEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(emailText, passText).addOnCompleteListener(this) {
+                    task -> if (task.isSuccessful) {
+                Toast.makeText(baseContext, "Login 成功", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(baseContext, "Login 失敗", Toast.LENGTH_SHORT).show()
+            }
+            }
+        }
+    }
+}
