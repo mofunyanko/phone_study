2fef1019098138d074f3c5f1bc32a256c280a6f2
mofunyanko <emiho.iwasaki@gmail.com>
2022-07-01 16:24:56 +0900

    Firestoreのデータやり取り成功

--- FirestorePractice/app/src/main/java/com/example/firestorepractice/MainActivity.kt
+++ FirestorePractice/app/src/main/java/com/example/firestorepractice/MainActivity.kt
@@ -2,9 +2,20 @@ package com.example.firestorepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firestorepractice.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val ADD_TAG = "add_task"
+        private const val READ_TAG = "read_task"
+        private const val TASKS_PATH = "tasks"
+    }
+
     // bindingクラスの変数
     private lateinit var binding: ActivityMainBinding
 
@@ -14,5 +25,51 @@ class MainActivity : AppCompatActivity() {
         // レイアウトを読み込む
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Firestoreをインスタンス化
+        val db = Firebase.firestore
+
+        // 追加ボタンを押したら
+        binding.addButton.setOnClickListener {
+            // Taskをインスタンス化
+            val task = Task(title = binding.titleEditText.text.toString())
+
+            // Firestoreに格納
+            db.collection("tasks")
+                .add(task)
+                .addOnSuccessListener { documentReference -> Log.d(ADD_TAG,"DocumentSnapshot added with ID: ${documentReference.id}") }
+                .addOnFailureListener { e -> Log.w(ADD_TAG, "Error adding document", e) }
+        }
+
+        // RecycleViewの設定
+        val taskAdapter = TaskAdapter()
+        binding.recycleView.adapter = taskAdapter
+        binding.recycleView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        // アプリ起動時に保存されているデータを取得
+        db.collection("tasks")
+            .get()
+            .addOnSuccessListener { tasks -> val taskList = ArrayList<Task>()
+                tasks.forEach {taskList.add(it.toObject(Task::class.java)) }
+                taskAdapter.submitList(taskList)
+            }
+            .addOnFailureListener { exception -> Log.d(READ_TAG, "Error getting documents: ", exception) }
+
+        // データの変更をリアルタイムでアプリに反映
+        db.collection("tasks").addSnapshotListener { tasks, e ->
+            if (e != null ) {
+                Log.w(READ_TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (tasks != null) {
+                val taskList = ArrayList<Task>()
+                tasks.forEach { taskList.add(it.toObject(Task::class.java)) }
+                taskAdapter.submitList(taskList)
+            }
+            else {
+                Log.d(READ_TAG, "Current data: null")
+            }
+        }
     }
 }
--- FirestorePractice/app/src/main/java/com/example/firestorepractice/TaskAdapter.kt
+++ FirestorePractice/app/src/main/java/com/example/firestorepractice/TaskAdapter.kt
@@ -10,16 +10,19 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class TaskAdapter: ListAdapter<Task, TaskViewHolder>(diffUtilItemCallback) {
+    // Viewを生成し、生成したViewをViewHolderni格納し、戻り値として返す
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val view = TaskListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return TaskViewHolder(view)
     }
 
+    // その位置のTaskを取得し、ViewHolderからViewにTask情報をセット
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 }
 
+// 生成したViewを保持し、bindでTaskをViewに反映
 class TaskViewHolder(private val binding: TaskListItemBinding): RecyclerView.ViewHolder(binding.root) {
     fun bind(task: Task) {
         binding.titleTextView.text = task.title
@@ -27,11 +30,14 @@ class TaskViewHolder(private val binding: TaskListItemBinding): RecyclerView.Vie
     }
 }
 
+// Taskの差分確認
 private val diffUtilItemCallback = object : DiffUtil.ItemCallback<Task>() {
+    // 渡されたデータが同じ値か
     override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
         return oldItem == newItem
     }
 
+    // 渡されたデータが同じ項目か
     override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
         return oldItem.id == newItem.id
     }
